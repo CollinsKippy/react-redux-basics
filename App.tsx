fb209c20a7b729b9650a54899dce5c176a6387bf
CollinsKippy <115981895+CollinsKippy@users.noreply.github.com>
2023-01-31 21:43:17 +0300

    configure store and counterSlice

--- App.tsx
+++ App.tsx
@@ -4,8 +4,8 @@ import './style.css';
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>React Redux Basics</h1>
+      <p>With Bootstrap 5.0</p>
     </div>
   );
 }
---
+++ features/counter/counterSlice.tsx
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState: {
+    value: 0,
+  },
+  reducers: {
+    increment: (state) => ({ ...state, value: state.value + 1 }),
+    decrement: (state) => ({ ...state, value: state.value - 1 }),
+  },
+});
